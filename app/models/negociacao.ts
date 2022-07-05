a63a70f08623457571c78b4495686af61fda8510
Yuri <yurifdev@gmail.com>
2022-07-05 18:34:34 -0300

    Adicionando configurações do typescript

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -2,41 +2,61 @@
 //     #data;
 //     #quantidade;
 //     #valor;
+
 //     constructor(data, quantidade, valor) {
 //         this.#data = data;
 //         this.#quantidade = quantidade;
 //         this.#valor = valor;
 //     }
+
 //     get data() {
 //         return this.#data
+
 //     }
 //     get quantidade() {
 //         return this.#quantidade
+
 //     }
 //     get valor() {
 //         return this.#valor
+
 //     }
+
 //     get volume() {
 //         return this.#quantidade * this.#valor;
+
 //     }
+
 // }
+
 // Remover a hashtag, pois ela é syntax JS, a equipe TS recomenda usar
 export class Negociacao {
+    private _data;
+    private _quantidade;
+    private _valor;
+
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     get data() {
-        return this._data;
+        return this._data
+
     }
     get quantidade() {
-        return this._quantidade;
+        return this._quantidade
+
     }
     get valor() {
-        return this._valor;
+        return this._valor
+
     }
+
     get volume() {
         return this._quantidade * this._valor;
+
     }
+
 }
