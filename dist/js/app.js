2afb45684f99f9613318704a722dda745faedc65
Yuri <yurifdev@gmail.com>
2022-07-06 21:40:25 -0300

    Form adicionado

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,3 +7,14 @@
 // // negociacao.quantidad = 12321312321;
 // // console.log(negociacao.quantidad);
 // console.log(negociacao.quantidade);
+//Capítulo 3
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    //(event: Event), por baixo dos panos. 
+    //Mas automaticamente já é inferido que a função é baseada num evento do tipo submit
+    //Por conta do addEventListenner
+    event.preventDefault();
+    controller.adiciona();
+});
