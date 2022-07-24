c3b2720cd49beb26c077e61c3489f85088ad1b50
Yuri <yurifdev@gmail.com>
2022-07-23 22:15:42 -0300

    Decoration feito, primeira versao

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -5,6 +5,18 @@ export function logarTempoDeExecucacao() {
         propertyKey: string,
         descriptor: PropertyDescriptor) 
     {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function(){
+            const t1  = performance.now();
+
+            //Chama o método original
+            const retorno = metodoOriginal()
+
+            const t2  = performance.now();
+            console.log(`${propertyKey}, tempo de execucação: ${(t2-t1)/1000}`);
+            
+            retorno
+        }
         return descriptor;
 
     }
